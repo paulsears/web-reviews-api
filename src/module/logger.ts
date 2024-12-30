4573e0794438edb861b73147de80b864a6252b0a
Michael Bond <MichaelRBond@users.noreply.github.com>
2024-12-30 10:50:15 -0500

    chore: Update log module instantiation (#9)

--- src/module/logger.ts
+++ src/module/logger.ts
@@ -1,6 +1,8 @@
-import { ApfmNestJsLogModule, Logger } from "@aplaceformom/apfm-logger-typescript";
+import { getApfmNestJsLogModule, Logger } from "@aplaceformom/apfm-logger-typescript";
 import { Global, Module } from "@nestjs/common";
 import { applicationConfig } from "../config";
+
+const ApfmNestJsLogModule = getApfmNestJsLogModule();
 
 @Global()
 @Module({
