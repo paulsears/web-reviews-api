419a33d4d80b8772784622ad22d496af6661f21e
Urbano Gardun <ugardun@gmail.com>
2025-05-12 14:23:00 +1000

    feat: modify files for easier templating when doing migrations

--- infrastructure/terraform/dev/main.tf
+++ infrastructure/terraform/dev/main.tf
@@ -1,8 +1,10 @@
 module "${YOUR_SERVICE_NAME}" {
-  source            = "git::git@github.com:aplaceformom/terraform-modules.git//modules/fargate-service?ref=v0.1.0"
+  source            = "git::git@github.com:aplaceformom/terraform-modules.git//modules/fargate-service?ref=v0.3.0"
   name              = "${YOUR_SERVICE_NAME}"
   cluster_name      = "default"
   container_port    = 4000
   public_access     = false
   health_check_path = "/health"
+
+  deployment_slot = "${DEPLOYMENT_SLOT}"
 }
